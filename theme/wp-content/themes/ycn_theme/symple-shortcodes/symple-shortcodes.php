8e41b89dfe08edeed3660f4af780e14d4b5374aa
Yassine BR <yassinebrahni@gmail.com>
2016-12-07 00:48:44 +0000

    Changement template HTML

--- theme/wp-content/themes/ycn_theme/symple-shortcodes/symple-shortcodes.php
+++
@@ -1,4 +0,0 @@
-<?php
-include(get_template_directory().'/symple-shortcodes/includes/scripts.php');
-include(get_template_directory().'/symple-shortcodes/includes/shortcode-functions.php');
-include(get_template_directory().'/symple-shortcodes/includes/mce/symple_shortcodes_tinymce.php');
